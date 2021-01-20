f9ce999d24b9ddbfe2776f60b57f07f55eee8f84
Sefer <Sefer@DESKTOP-5O93JGC>
2021-01-20 23:50:39 +0300

    20.01.2021 ders sonu

---
+++ OOP1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "masa";
+            product1.UnitPrice = 500;
+            product1.UnitInStock = 3;
+
+            Product product2 = new Product
+            {
+                Id = 2,
+                CategoryId = 5,
+                ProductName = "kalem",
+                UnitInStock = 5,
+                UnitPrice = 35
+            };
+            //ProductManager productManager = new ProductManager();
+            //productManager.Add(product1);
+
+            //productManager.Topla2(3, 6);
+            //int toplamasonucu = productManager.Topla(3, 6);
+            //Console.WriteLine(toplamasonucu*2);
+        }
+    }
+}
