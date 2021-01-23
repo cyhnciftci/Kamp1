3ea1b51a76c60c7f11de5b1e77f606c6f6195f4b
Sefer <Sefer@DESKTOP-5O93JGC>
2021-01-23 23:51:31 +0300

    23.01.2021

---
+++ OOP3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            İhtiyacKrediManager ihtiyacKrediManager = new İhtiyacKrediManager();
+           // ihtiyacKrediManager.Hesapla();
+            //TasitKrediManager tasitKrediManager = new TasitKrediManager();
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            //tasitKrediManager.Hesapla();
+            //iki şekilde olur
+            IKrediManager konutKrediManager = new KonutKrediManager();
+            // konutKrediManager.Hesapla();
+            BasvuruManager basvuruManager = new BasvuruManager();
+            IloggerService databaseLoggerService = new DatabaseLoggerService();
+            IloggerService fileBaseLoggerService = new FilebaseLoggerService();
+            basvuruManager.BasvuruYap(konutKrediManager, databaseLoggerService);
+            basvuruManager.BasvuruYap(tasitKrediManager, fileBaseLoggerService);
+            basvuruManager.BasvuruYap(ihtiyacKrediManager, new DatabaseLoggerService());
+
+            basvuruManager.BasvuruYap(new EsnafKredisiManager(), new SmsLoggerService());
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager, tasitKrediManager, konutKrediManager };
+
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+
+
+        }
+
+    }
+}
---
+++ Oop2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Oop2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//engin demiroğ
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "engin";
+            musteri1.Soyadi = "demiroğ";
+            musteri1.TcNo = "12345678";
+            musteri1.Id = 1;
+
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "54321";
+            musteri2.SirketAdi = "kodlama.io";
+            musteri2.VergiNo = "87654321";
+
+            //gerçek müsşteri-tüzel müşteri
+            //solid
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+
+        }
+    }
+}
